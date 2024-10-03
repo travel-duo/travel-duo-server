f11300a69a440540542c9dc584f031db7fb49ea0
이윤건(Macko) <sangok1993@gmail.com>
2024-10-03 15:23:32 +0900

    config로 설정파일 이도

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,8 +12,8 @@ import * as process from 'process';
       isGlobal: true, // 전역으로 ConfigModule을 사용할 수 있게 합니다.
       envFilePath:
         process.env.NODE_ENV === 'production'
-          ? ['.env.production', '.env']
-          : ['.env.local', '.env'],
+          ? ['config/.env.production', 'config/.env']
+          : ['config/.env.local', 'config/.env'],
     }),
     AuthModule,
     DatabaseModule,
